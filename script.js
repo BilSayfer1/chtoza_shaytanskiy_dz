528ceabd9e2a43d570236053f67e27390d30252e
Al Safir <142802154+BilSayfer1@users.noreply.github.com>
2024-06-10 17:40:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let todos = [
+    {
+        time: new Date().toDateString(),
+    }
+];
+
+function openModal() {
+   let task_modal = document.getElementById("task_modal")
+   task_modal.style.display = "block";
+}
+
+function closeModal() {
+    let task_modal = document.getElementById("task_modal")
+    task_modal.style.display = "none"
+
+}
+
+function addTask(e) {
+    e.preventDefault();
+
+    const taskName = document.getElementById("task_name").value;
+    const taskCategory = document.getElementById("task_category").value;
+    const taskOptions = document.getElementById("task_options").value
+    const taskDate = todos[0].time;
+
+    const newTaskCategory = document.createElement("div");
+    newTaskCategory.className = "task-category";
+    newTaskCategory.innerHTML = `
+        <h2 class="osnova">${taskOptions}</h2>
+        <ul>
+            <li>
+                <textarea>${taskName}</textarea>
+                <br>
+                <span class="due-date">Due: ${taskDate}</span>
+                <button class="view-btn"><img src="./img/icons8-редактировать-50.png" alt=""></button>
+                <button class="delete-btn"><img src="./img/icons8-кнопка-удалить-50.png" alt=""></button>
+            </li>
+        </ul>
+    `;
+
+   let box = document.querySelector(".box")
+   box.append(newTaskCategory);
+
+  let task_form = document.getElementById("task_form")
+  task_form.reset();
+    closeModal();
+}
